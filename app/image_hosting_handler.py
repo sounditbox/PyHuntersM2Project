43c3a524fdd9b6aca53ec95c690adf55a3752121
Ruslan <sounditbox@gmail.com>
2026-05-07 21:03:43 +0400

    fix: refactor get of IHH

--- app/image_hosting_handler.py
+++ app/image_hosting_handler.py
@@ -18,26 +18,20 @@ class ImageHostingHandler(BaseHandler):
     def do_GET(self):
         logger.info(f"GET {self.client_address[0]}: {self.path}")
 
-        if self.path.startswith('/api/'):
-            if self.path == '/api/images':
-                self.get_images_names()
-            # get all data from db of images
-            elif self.path == '/api/images-data/':
-                self.get_images()
-            elif self.path.startswith('/api/images/'):
-                name = self.path.split('/')[-1]
-                self.send_media_file(name)
+        api_handlers = {
+            '/api/images': self.get_images_names,
+            '/api/images-data/': self.get_images,
+        }
+        templates = {
+            '/': 'index.html',
+            '/upload': 'upload.html',
+            '/images': 'images.html'
+        }
 
-        elif self.path == '/':
-            self.template_response('index.html')
-        elif self.path == '/upload':
-            self.template_response('upload.html')
-        elif self.path == '/images':
-            self.template_response('images.html')
-        # images-list
-        #
-        # elif any((self.path.endswith(ext) for ext in ['.css', '.js', '.png'])):
-        #     self.send_static_file(self.path)
+        if self.path in templates:
+            self.template_response(templates[self.path])
+        elif self.path in api_handlers:
+            api_handlers[self.path]()
         else:
             self.html_response('Not Found', 404)
 
